88bf3f2adb19902bb3150dc2eb724dc5fa63999f
solidados <geekbrains.pk@internet.ru>
2024-06-01 22:26:25 +0400

    feat: create connection to DB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./db");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const start = async () => {
+  try {
+    await sequelize.authenticate(); /** this is to connect to my DB */
+    await sequelize.sync(); /** this will sync my DB with DB schema for any changes */
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+start();
