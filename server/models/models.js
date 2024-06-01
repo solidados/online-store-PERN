c04ec716655d153ef632f7078164199df7c46e9d
solidados <geekbrains.pk@internet.ru>
2024-06-01 22:43:33 +0400

    feat: add Basket, BasketProduct, Product models according to DB diagram

--- server/models/models.js
+++ server/models/models.js
@@ -7,3 +7,19 @@ const User = sequelize.define("user", {
   password: { type: DataTypes.STRING },
   role: { type: DataTypes.STRING, defaultValue: "USER" },
 });
+
+const Basket = sequelize.define("basket", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const BasketProduct = sequelize.define("basket_product", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const Product = sequelize.define("product", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+  price: { type: DataTypes.INTEGER, unique: true, allowNull: false },
+  rating: { type: DataTypes.INTEGER, defaultValue: 0 },
+  img: { type: DataTypes.STRING, allowNull: false },
+});
