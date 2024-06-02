6923af6c2596b9e08de7741be718c51704479020
solidados <geekbrains.pk@internet.ru>
2024-06-02 13:51:10 +0400

    feat: import CORS to be able to send requests from client side to server side

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ import dotenv from 'dotenv';
 import express from 'express';
 import sequelize from './db.js';
 import models from './models/models.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const start = async () => {
   try {
