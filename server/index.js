687877432e934f73ea529cc52f30ea77612c3238
solidados <geekbrains.pk@internet.ru>
2024-06-02 14:01:16 +0400

    feat: import router to entry point index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import sequelize from './db.js';
 import models from './models/models.js';
 import cors from 'cors';
+import router from './routes/index.js';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
 
 const start = async () => {
   try {
