e5315261ed21434693cd4ad89f0e6ad34d279efb
solidados <geekbrains.pk@internet.ru>
2024-06-02 16:16:41 +0400

    feat: create requests for brandController

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -1,11 +1,16 @@
+import { Brand } from '../models/models.js';
+import ApiError from "../errors/ApiError.js";
 
 class BrandController {
   async create(req, res) {
-
+    const { name } = req.body;
+    const brand = await Brand.create({ name });
+    return res.json(brand);
   }
 
   async getAll(req, res) {
-
+    const brands = await Brand.findAll();
+    return res.json(brands);
   }
 }
 
